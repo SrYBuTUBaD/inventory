b0fe636123a295708fced39fec8d809590537c4b
SrYBuTUBaD <121572728+SrYBuTUBaD@users.noreply.github.com>
2026-06-29 07:55:12 +0200

    Delete esx_inventory directory

--- esx_inventory/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-Config.MaxWeight     = 50000   -- Maximales Gewicht Rucksack in Gramm (50 KG = 50000)
-Config.TrunkWeight   = 350000  -- Maximales Gewicht Kofferraum in Gramm (350 KG)
-Config.Slots         = 20      -- Anzahl Slots im Rucksack
-Config.TrunkSlots    = 16      -- Anzahl Slots im Kofferraum
