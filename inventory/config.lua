18dbc461f13d0cb961c7a33bb859c87eebedaa57
SrYBuTUBaD <121572728+SrYBuTUBaD@users.noreply.github.com>
2026-06-30 05:50:34 +0200

    Add files via upload

---
+++ inventory/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.MaxWeight     = 20000   -- Maximales Gewicht Rucksack in Gramm (1 KG = 1000)
+Config.TrunkWeight   = 350000  -- Maximales Gewicht Kofferraum in Gramm (350 KG)
+Config.Slots         = 6       -- Anzahl Slots im Rucksack
+Config.TrunkSlots    = 16      -- Anzahl Slots im Kofferraum
---
+++ inventory/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name        'inventory'
+description 'Custom Inventory'
+author      'Kylian'
+version     '1.0.0'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/app.js',
+    'html/img/items/*.png'
+}
